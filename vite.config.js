d092063e5cbe63e771b40c408a94a8e90fb33aec
iannico322 <73187566+iannico322@users.noreply.github.com>
2022-11-27 19:12:08 +0800

    asd

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  base:'https://iannico322.github.io/test/',
+
 })
